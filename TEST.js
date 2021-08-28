6874402aa4b008e1fa3cb033dd89114a4b4f79bd
phong260201 <87638762+phong260201@users.noreply.github.com>
2021-08-28 19:45:54 +0700

    first commit

---
+++ TEST.js
@@ -0,0 +1,15 @@
+document.addEventListener("mousemove", (e) => {
+    let body = document.querySelector('body')
+    let heart = document.createElement('span')
+    let x = e.offsetX;
+    let y = e.offsetY;
+    heart.style.left = x + "px";
+    heart.style.top = y + "px";
+    let size = Math.random() * 100;
+    heart.style.width = size + "px";
+    heart.style.height = size + "px";
+    body.appendChild(heart);
+    setTimeout(() => {
+        heart.remove();
+    }, 3000)
+})
